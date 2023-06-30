c9ea26182c82d685254940cd7d9013566e8b435e
arpitsalt <desynchronizedentity@gmail.com>
2023-06-30 17:03:06 +0530

    updated the app

--- main.py
+++ main.py
@@ -1,47 +1,54 @@
-import streamlit as st
+import stqdm
+import pickle
+import pandas as pd
 from PIL import Image
+from time import sleep
+import streamlit as st
+from stqdm import stqdm
+import random
 from streamlit_option_menu import option_menu
 
-st.set_page_config(page_title="Omdena Rwanda", page_icon=":rwanda:", initial_sidebar_state="expanded")
-img_banner = Image.open("assets/banner.png")
-img_banner2 = Image.open("assets/banner2.png")
-img_rwanda = Image.open("assets/rwanda-logo.png")
 
+# Configuring Streamlit
+st.set_page_config(page_title="Omdena Rwanda", page_icon=":rwanda:", initial_sidebar_state="expanded")
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
             """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 css_style = {
     "icon": {"color": "white"},
     "nav-link": {"--hover-color": "grey"},
     "nav-link-selected": {"background-color": "#FF4C1B"},
 }
 
+# Loading assets
+img_banner = Image.open("assets/images/banner.png")
+img_banner2 = Image.open("assets/images/banner2.png")
+img_rwanda = Image.open("assets/images/rwanda-logo.png")
+
 
 def home_page():
     st.write(f"""# Water Inspection System""", unsafe_allow_html=True)
     st.image(img_banner)
 
-    st.write(f"""<h1>The Problem</h1> 
-            <p>Access to clean water is a critical challenge in many parts of the world, 
-            including Rwanda. Water quality prediction is important for ensuring the availability of safe and clean water for 
-            drinking, agriculture, and other purposes. However, traditional methods for water quality prediction are often 
-            time-consuming and costly, and they may not provide accurate and timely information. To address this challenge, 
-            the Omdena Rwanda Chapter has initiated a project to develop an automated water quality prediction system using 
-            machine learning.</p> """, unsafe_allow_html=True)
+    st.write(f"""<h1>The Problem</h1> <p>Access to clean water is a critical challenge in many parts of the world, 
+    including Rwanda. Water quality prediction is important for ensuring the availability of safe and clean water for 
+    drinking, agriculture, and other purposes. However, traditional methods for water quality prediction are often 
+    time-consuming and costly, and they may not provide accurate and timely information. To address this challenge, 
+    the Omdena Rwanda Chapter has initiated a project to develop an automated water quality prediction system using 
+    machine learning.</p> """, unsafe_allow_html=True)
 
     st.write(f"""<h1>Project goals</h1> <p>In this project, the Omdena Rwanda Chapter’s primary goal in this project 
-            is to develop an accurate and efficient machine learning model that can predict water quality based on a range of 
-            parameters such as Electrical conductivity of water, Amount of organic carbon in ppm, Amount of Trihalomethanes 
-            in μg/L, and turbidity. The model will be trained on a large dataset of historical water quality data and will be 
-            designed to provide predictions for water quality..</p> """, unsafe_allow_html=True)
+    is to develop an accurate and efficient machine learning model that can predict water quality based on a range of 
+    parameters such as Electrical conductivity of water, Amount of organic carbon in ppm, Amount of Trihalomethanes 
+    in μg/L, and turbidity. The model will be trained on a large dataset of historical water quality data and will be 
+    designed to provide predictions for water quality..</p> """, unsafe_allow_html=True)
 
 
-def about_section():
+def about_page():
     st.write(f"""<h1>Project background</h1>""", unsafe_allow_html=True)
     st.write("""
         <p>Rwanda is a landlocked country located in East Africa, 
@@ -59,9 +66,78 @@ def about_section():
     st.image(img_banner2)
 
 
+def model_section():
+    st.write('# Predict Water Quality')
+    st.caption('Set these values of these parameters to know if the water quality is suitable to drink or not.')
 
-def model_page():
-    st.error("No models to show for now.")
+    col1, col2, col3 = st.columns(3, gap="large")
+
+    # setting random values
+    for i in range(17):
+        st.session_state[f"test_slider{i}"] = random.randint(0, 1000)
+
+    with col1:
+        ColourTCU = st.slider(label="Colour (TCU)", min_value=0, max_value=1000, value=None, step=1, format="%f",
+                              key="test_slider0")
+        TurbidityNTU = st.slider(label="Turbidity (NTU)", min_value=0, max_value=1000, value=None, step=1, format="%f",
+                                 key="test_slider1")
+        pH = st.slider(label="pH", min_value=0, max_value=1000, value=None, step=1, format="%f", key="test_slider2")
+        ConductivityuS = st.slider(label="Conductivity (uS/cm)", min_value=0, max_value=1000, value=None, step=1,
+                                   format="%f", key="test_slider3")
+        TotalDissolvedSolids = st.slider(label="Total Dissolved Solids (mg/l)", min_value=0, max_value=1000, value=None,
+                                         step=1, format="%f", key="test_slider4")
+        TotalHardness = st.slider(label="Total Hardness (mg/l as CaCO3)", min_value=0, max_value=1000, value=None,
+                                  step=1, format="%f", key="test_slider5")
+
+    with col2:
+        Aluminium = st.slider(label="Aluminium (mg/l)", min_value=0, max_value=1000, value=None, step=1, format="%f",
+                              key="test_slider6")
+        Chloride = st.slider(label="Chloride (mg/l)", min_value=0, max_value=1000, value=None, step=1, format="%f",
+                             key="test_slider7")
+        Iron = st.slider(label="Iron (mg/l)", min_value=0, max_value=1000, value=None, step=1, format="%f",
+                         key="test_slider8")
+        Sodium = st.slider(label="Sodium (mg/l)", min_value=0, max_value=1000, value=None, step=1, format="%f",
+                           key="test_slider9")
+        Sulphate = st.slider(label="Sulphate (mg/l)", min_value=0, max_value=1000, value=None, step=1, format="%f",
+                             key="test_slider10")
+        Zinc = st.slider(label="Zinc (mg/l)", min_value=0, max_value=1000, value=None, step=1, format="%f",
+                         key="test_slider11")
+
+    with col3:
+        Magnesium = st.slider(label="Magnesium (mg/l)", min_value=0, max_value=1000, value=None, step=1, format="%f",
+                              key="test_slider12")
+        Calcium = st.slider(label="Calcium (mg/l)", min_value=0, max_value=1000, value=None, step=1, format="%f",
+                            key="test_slider13")
+        Potassium = st.slider(label="Potassium (mg/l)", min_value=0, max_value=1000, value=None, step=1, format="%f",
+                              key="test_slider14")
+        Nitrate = st.slider(label="Nitrate (mg/l)", min_value=0, max_value=1000, value=None, step=1, format="%f",
+                            key="test_slider15")
+        Phosphate = st.slider(label="Phosphate (mg/l)", min_value=0, max_value=1000, value=None, step=1, format="%f",
+                              key="test_slider16")
+        st.write("<br>", unsafe_allow_html=True)
+        predict_button = st.button('  Predict Water Quality  ')
+
+    dataframe = pd.DataFrame({'Colour (TCU)': [ColourTCU], 'Turbidity (NTU)': [TurbidityNTU], 'pH': [pH],
+                              'Conductivity (uS/cm)': [ConductivityuS],
+                              'Total Dissolved Solids (mg/l)': [TotalDissolvedSolids],
+                              'Total Hardness (mg/l as CaCO3)': [TotalHardness], 'Aluminium (mg/l)': [Aluminium],
+                              'Chloride (mg/l)': [Chloride], 'Total Iron (mg/l)': [Iron],
+                              'Sodium (mg/l)': [Sodium], 'Sulphate (mg/l)': [Sulphate], 'Zinc (mg/l)': [Zinc],
+                              'Magnesium (mg/l)': [Magnesium], 'Calcium (mg/l)': [Calcium],
+                              'Potassium (mg/l)': [Potassium], 'Nitrate (mg/l)': [Nitrate],
+                              'Phosphate (mg/l)': [Phosphate]})
+
+    with open('assets/model.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+    if predict_button:
+        result = model.predict(dataframe)
+        for _ in stqdm(range(50)):
+            sleep(0.015)
+        if result[0] == 1.0:
+            st.error("This Water Quality is Non-Potable")
+        else:
+            st.success('This Water Quality is Potable')
 
 
 def contributors_page():
@@ -91,10 +167,10 @@ if selected == "Home":
     home_page()
 
 if selected == "About":
-    about_section()
+    about_page()
 
 if selected == "Model":
-    model_page()
+    model_section()
 
 if selected == "Contributors":
     st.write("## A heart felt thankyou to all of our contributors <br><br>", unsafe_allow_html=True)
