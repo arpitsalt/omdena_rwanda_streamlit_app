83fceb8b7a66d80d30c6f206f0b6b5d69b42f92c
Arpit <arpitsengar99@gmail.com>
2023-07-08 20:27:27 +0530

    changed maximum and step value

--- main.py
+++ main.py
@@ -92,11 +92,11 @@ def model_section():
                                         step=50.0, format="%f", key="test_slider5")
 
     with col2:
-        Aluminium = st.number_input(label="Aluminium (mg/l)", min_value=0.0, max_value=1000.0, step=50.0, format="%f",
+        Aluminium = st.number_input(label="Aluminium (mg/l)", min_value=0.0, max_value=1000.5, step=50.1, format="%f",
                                     key="test_slider6")
         Chloride = st.number_input(label="Chloride (mg/l)", min_value=0.0, max_value=1000.0, step=50.0, format="%f",
                                    key="test_slider7")
-        Iron = st.number_input(label="Iron (mg/l)", min_value=0.0, max_value=1000.0, step=50.0, format="%f",
+        Iron = st.number_input(label="Iron (mg/l)", min_value=0.0, max_value=1000.5, step=50.1, format="%f",
                                key="test_slider8")
         Sodium = st.number_input(label="Sodium (mg/l)", min_value=0.0, max_value=1000.0, step=50.0, format="%f",
                                  key="test_slider9")
@@ -114,7 +114,7 @@ def model_section():
                                     key="test_slider14")
         Nitrate = st.number_input(label="Nitrate (mg/l)", min_value=0.0, max_value=1000.0, step=50.0, format="%f",
                                   key="test_slider15")
-        Phosphate = st.number_input(label="Phosphate (mg/l)", min_value=0.0, max_value=1000.0, step=50.0, format="%f",
+        Phosphate = st.number_input(label="Phosphate (mg/l)", min_value=0.0, max_value=1000.2, step=50.1, format="%f",
                                     key="test_slider16")
         st.write("<br>", unsafe_allow_html=True)
         predict_button = st.button('  Predict Water Quality  ')
